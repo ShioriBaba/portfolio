68d91feb61cf91678c069a149d7656577a66942d
ShioriBaba <71863746+ShioriBaba@users.noreply.github.com>
2020-10-12 12:51:15 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+
+$('#aboutmenu').click(function() {
+$('.site a [href="about"]').click(function(){
+var speed=500;
+var target=$("#about");
+
+$('#workmenu').click(function() {
+$('.site a [href="works"]').click(function(){
+var speed=500;
+var target=$("#works");
+
+$('.slider').slick();
